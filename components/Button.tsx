a5677c0c406c9e21eb93831c80391dde929578f7
Sami <sradwan93@gmail.com>
2025-01-18 16:29:02 -0500

    added post functionality

--- components/Button.tsx
+++ components/Button.tsx
@@ -32,10 +32,10 @@ const Button: React.FC<ButtonProps> = ({
             ${fullWidth ? 'w-full' : 'w-fit'}
             ${secondary ? 'bg-white' : 'bg-sky-500'}
             ${secondary ? 'text-black' : 'text-white'}
-            ${secondary ? 'border-black' : 'bg-sky-500'}
-            ${large ? 'text-xl' : 'bg-sky-500'}
+            ${secondary ? 'border-black' : 'border-sky-500'}
+            ${large ? 'text-xl' : 'text-md'}
             ${large ? 'px-5' : 'px-4'}
-            ${large ? 'py-3' : 'px-2'}
+            ${large ? 'py-3' : 'py-2'}
             ${outline ? 'bg-transparent' : ''}
             ${outline ? 'border-white' : ''}
             ${outline ? 'text-white' : ''}
